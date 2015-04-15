919223e656ff6e048692cfc0cacb1ba2999a0bd9
Administrator <lili.cheng@awit.net.cn>
2015-04-15 18:38:05 +0800

    将CJDownlineImpl更新为和bean目录下的CJDownline实体类类型一致。

--- app/src/main/java/rwcjom/awit/com/rwcjo_m/implInterfaces/CJDownlineImpl.java
+++ app/src/main/java/rwcjom/awit/com/rwcjo_m/implInterfaces/CJDownlineImpl.java
@@ -10,9 +10,12 @@ import org.ksoap2.serialization.SoapSerializationEnvelope;
 import org.ksoap2.transport.HttpTransportSE;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import rwcjom.awit.com.rwcjo_m.bean.CJDownline;
+import rwcjom.awit.com.rwcjo_m.util.CommonTools;
 import rwcjom.awit.com.rwcjo_m.util.ValueConfig;
 import rwcjom.awit.com.rwcjo_m.bean.pubUtil;
 import rwcjom.awit.com.rwcjo_m.interfaces.CJDownlineInterface;
@@ -20,53 +23,64 @@ import rwcjom.awit.com.rwcjo_m.bean.Bw;
 
 
 public class CJDownlineImpl implements CJDownlineInterface {
+	private String TAG="CJDownlineImpl";
 	private String result;
 		@Override
 		public void getCJDownline(String sectid, String startdate, String enddate,
 				String randomcode) {
-			// 命名空间
-			String nameSpace = "http://www.xfiretest.com/";
-
-			// 调用的方法名称
-			String methodName = "CJDownline";
-			// EndPoint
-			String endPoint = ValueConfig.ENDPOINT_STRING;
-			// SOAP Action
-			String soapAction = "http://www.xfiretest.com/CJDownline";
-
-			// 指定WebService的命名空间和调用的方法名
-			SoapObject rpc = new SoapObject(nameSpace, methodName);
-			// 设置需调用WebService接口需要传入的两个参数mobileCode、userId
-			Log.i("randomcode",randomcode);
-			rpc.addProperty("sectid", sectid);
-			rpc.addProperty("startdate", startdate);
-			rpc.addProperty("enddate", enddate);
-			rpc.addProperty("randomcode", randomcode);
-			// 生成调用WebService方法的SOAP请求信息,并指定SOAP的版本
-			SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(
-					SoapEnvelope.VER10);
-
-			envelope.bodyOut = rpc;
-			// 设置是否调用的是dotNet开发的WebService
-			envelope.dotNet = true;
-			// 等价于envelope.bodyOut = rpc;
-			envelope.setOutputSoapObject(rpc);
-
-			HttpTransportSE transport = new HttpTransportSE(endPoint);
-			try {
-				// 调用WebService
-				transport.call(soapAction, envelope);
-
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
+//			// 命名空间
+//			String nameSpace = "http://www.xfiretest.com/";
+//
+//			// 调用的方法名称
+//			String methodName = "CJDownline";
+//			// EndPoint
+//			String endPoint = ValueConfig.ENDPOINT_STRING;
+//			// SOAP Action
+//			String soapAction = "http://www.xfiretest.com/CJDownline";
+//
+//			// 指定WebService的命名空间和调用的方法名
+//			SoapObject rpc = new SoapObject(nameSpace, methodName);
+//			// 设置需调用WebService接口需要传入的两个参数mobileCode、userId
+//			Log.i("randomcode",randomcode);
+//			rpc.addProperty("sectid", sectid);
+//			rpc.addProperty("startdate", startdate);
+//			rpc.addProperty("enddate", enddate);
+//			rpc.addProperty("randomcode", randomcode);
+//			// 生成调用WebService方法的SOAP请求信息,并指定SOAP的版本
+//			SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(
+//					SoapEnvelope.VER10);
+//
+//			envelope.bodyOut = rpc;
+//			// 设置是否调用的是dotNet开发的WebService
+//			envelope.dotNet = true;
+//			// 等价于envelope.bodyOut = rpc;
+//			envelope.setOutputSoapObject(rpc);
+//
+//			HttpTransportSE transport = new HttpTransportSE(endPoint);
+//			try {
+//				// 调用WebService
+//				transport.call(soapAction, envelope);
+//
+//			} catch (Exception e) {
+//				e.printStackTrace();
+//			}
 			SoapObject object = null;
 			JSONArray jsonLine;
 			JSONObject jsonObj;
 
 			try {
-				pubUtil.downlines.clear();
-				object = (SoapObject) envelope.bodyIn;
+				Log.i(TAG, randomcode);
+				String methodNameString="CJDownline";
+				Map<String,String> paramsvalue=new HashMap<String,String>();
+				paramsvalue.put("sectid",sectid);
+				paramsvalue.put("randomcode",randomcode);
+				object=CommonTools.getObject(methodNameString,paramsvalue);
+				if(object ==null){
+					Log.i(TAG, "Object is null");
+				}
+				if(pubUtil.downlines.size()>0){
+					pubUtil.downlines.clear();
+				}
 				for(int i =0;i<object.getPropertyCount();i++){
 					result = object.getProperty(i).toString();
 					jsonLine=new JSONArray(result);
@@ -105,6 +119,10 @@ public class CJDownlineImpl implements CJDownlineInterface {
 						}
 					}
 				}
+			} catch(NullPointerException e){
+				e.printStackTrace();
+				Log.i(TAG, "空指针异常");
+				pubUtil.exception.setExceptionMsg("空指针异常");
 			} catch (Exception e) {
 				pubUtil.exception.setFlag(-5);
 				Log.i("exception", pubUtil.exception.getFlag()+"");
@@ -128,7 +146,6 @@ public class CJDownlineImpl implements CJDownlineInterface {
 					Log.i("exception","6");
 				}
 			}
-//				Log.i("exception", pubUtil.exception.getExceptionMsg());
 		}
 	}
 
