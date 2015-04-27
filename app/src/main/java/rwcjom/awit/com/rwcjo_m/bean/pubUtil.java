f688f2ef0fc93c2d4701ad06f9beff35c7f3dd25
Administrator <lili.cheng@awit.net.cn>
2015-04-27 09:18:25 +0800

    删除公有类里的接口初始化。

--- app/src/main/java/rwcjom/awit/com/rwcjo_m/bean/pubUtil.java
+++ app/src/main/java/rwcjom/awit/com/rwcjo_m/bean/pubUtil.java
@@ -8,13 +8,5 @@ public class pubUtil {
 	public static verifyAppUser verifyApp=new verifyAppUser();
 	public static getPublicKey getPubKey=new getPublicKey();
 	public static exceptionHandle exception=new exceptionHandle();
-	public static List<CJDownsectsite> downsectsites=new ArrayList<CJDownsectsite>();
-	public static List<CJDownface> downfaces=new ArrayList<CJDownface>();
-	public static List<CJDownfaceinfo> downfaceinfos=new ArrayList<CJDownfaceinfo>();
-	public static List<CJDownline> downlines=new ArrayList<CJDownline>();
-	public static List<CJDownbasepnt> downbasepnts=new ArrayList<CJDownbasepnt>();
-	public static List<CJDownperson> downpersons=new ArrayList<CJDownperson>();
-	public static List<CJDownpntinfo> downpntinfos=new ArrayList<CJDownpntinfo>();
-	public static List<CJDownbrginfo> downbrginfos=new ArrayList<CJDownbrginfo>();
 	public static CJDownReturnCode downReturnCode=new CJDownReturnCode();
 }
