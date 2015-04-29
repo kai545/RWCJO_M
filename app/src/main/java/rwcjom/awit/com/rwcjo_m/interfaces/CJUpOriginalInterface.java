7b1a1a91f5a25c1d62197f3673966ef3ec4fe2c4
Administrator <lili.cheng@awit.net.cn>
2015-04-29 09:46:54 +0800

    将原始数据上传接口的返回值改为CJUpOriginal对象。

--- app/src/main/java/rwcjom/awit/com/rwcjo_m/interfaces/CJUpOriginalInterface.java
+++ app/src/main/java/rwcjom/awit/com/rwcjo_m/interfaces/CJUpOriginalInterface.java
@@ -4,13 +4,15 @@ import android.content.Context;
 
 import com.mor.dataacquisition.struct.BClass;
 
+import rwcjom.awit.com.rwcjo_m.bean.CJUpOriginal;
+
 /**
  * 原始观测数据上传API接口
  * @author Administrator
  *
  */
 public interface CJUpOriginalInterface {
-	public void getCJUpOriginal(BClass[] blist,
+	public CJUpOriginal getCJUpOriginal(BClass[] blist,
 			String equipbrand,String instrumodel,String serialnum,String sjid,String temperature,
 			String barometric,String weather,String benchmarkids,String mtype,
 			String mdate,String linecode,String account,
