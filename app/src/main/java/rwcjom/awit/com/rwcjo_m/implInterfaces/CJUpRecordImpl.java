624642a7118495947819ec4e772ced6bd3ec6675
Administrator <lili.cheng@awit.net.cn>
2015-04-29 15:36:36 +0800

    原始观测数据api接口

--- app/src/main/java/rwcjom/awit/com/rwcjo_m/implInterfaces/CJUpRecordImpl.java
+++ app/src/main/java/rwcjom/awit/com/rwcjo_m/implInterfaces/CJUpRecordImpl.java
@@ -1,6 +1,7 @@
 package rwcjom.awit.com.rwcjo_m.implInterfaces;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.mor.dataacquisition.DataAcquisition;
 import com.mor.dataacquisition.net.dataCallBacks.CJUpRecordInitDataCallBack;
@@ -20,7 +21,27 @@ public class CJUpRecordImpl implements CJUpRecordInterface {
     @Override
     public CJUpRecord getCJUpRecord(String bffb, String bfpcode, String bfpl, String bfpvalue, Context context) {
         cjupRecordObj=new CJUpRecord();
-
+        result=DataAcquisition.getInstance().CjUpRecord(bffb,bfpcode,bfpl,bfpvalue,context);
+        Log.i(TAG,result+"1");
+        if(result==0){
+            cjupRecordObj.setFlag(0);
+            cjupRecordObj.setResult(result);
+        }else if(result==1){
+            cjupRecordObj.setFlag(-1);
+            cjupRecordObj.setMsg("bffb有误");
+        }else if(result==2){
+            cjupRecordObj.setFlag(-1);
+            cjupRecordObj.setMsg("bfpcode有误");
+        }else if(result==3){
+            cjupRecordObj.setFlag(-1);
+            cjupRecordObj.setMsg("bfpl有误");
+        }else if(result==4){
+            cjupRecordObj.setFlag(-1);
+            cjupRecordObj.setMsg("bfpvalue有误");
+        }else if(result==-1){
+            cjupRecordObj.setFlag(-1);
+            cjupRecordObj.setMsg("其他错误");
+        }
         return cjupRecordObj;
     }
 }
