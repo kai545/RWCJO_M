d509e09d8a1b67bb62a9d968dbff44b10c914462
Administrator <lili.cheng@awit.net.cn>
2015-04-29 13:15:03 +0800

    修改初始化接口的代码

--- app/src/main/java/rwcjom/awit/com/rwcjo_m/implInterfaces/CJUpRecordInitImpl.java
+++ app/src/main/java/rwcjom/awit/com/rwcjo_m/implInterfaces/CJUpRecordInitImpl.java
@@ -8,6 +8,7 @@ import com.mor.dataacquisition.net.dataCallBacks.CJUpRecordInitDataCallBack;
 import com.mor.dataacquisition.net.parsedData.CJResutResult;
 
 import rwcjom.awit.com.rwcjo_m.bean.CJUpRecordInit;
+import rwcjom.awit.com.rwcjo_m.bean.pubUtil;
 import rwcjom.awit.com.rwcjo_m.interfaces.CJUpRecordInitInterface;
 
 /**
@@ -15,32 +16,35 @@ import rwcjom.awit.com.rwcjo_m.interfaces.CJUpRecordInitInterface;
  */
 public class CJUpRecordInitImpl implements CJUpRecordInitInterface {
     private String TAG="CJUpRecordInitImpl";
-    private CJUpRecordInit cjupRecordInitObj;
+//    private CJUpRecordInit cjupRecordInitObj;
     private Integer result=-5;
     @Override
     public void getCJUpRecordInit(final String account,final Context context,final CJUpRecordInitDataCallBack callBack) {
-        cjupRecordInitObj=new CJUpRecordInit();
-        Log.i("come on","jajajaaj");
-        DataAcquisition.getInstance().CjUpRecordInit(account,context,new CJUpRecordInitDataCallBack(){
+        //cjupRecordInitObj=new CJUpRecordInit();
+        Log.i("come on", "jajajaaj");
+        DataAcquisition.getInstance().CjUpRecordInit(account, context, new CJUpRecordInitDataCallBack() {
             @Override
             public void processData(CJResutResult data) {
+                Log.i("result:", result + "121");
                 super.processData(data);
-                result=data.returnCode;
-                Log.i("result:",result+"");
-                if(result==0){
-                    Log.i("进来没？？？","进来了 哈哈");
-                    cjupRecordInitObj.setResult(result);
-                    cjupRecordInitObj.setFlag(0);
-                }else if(result==-1){
-                    cjupRecordInitObj.setFlag(-1);
-                    cjupRecordInitObj.setMsg("其他错误");
-                }else if(result==-2){
-                    cjupRecordInitObj.setFlag(-1);
-                    cjupRecordInitObj.setMsg("account有误");
+                result = data.returnCode;
+                Log.i("result:", result + "");
+                if (result == 0) {
+                    Log.i("进来没？？？", "进来了 哈哈");
+                    pubUtil.upRecordInit.setResult(result);
+                    pubUtil.upRecordInit.setFlag(0);
+                } else if (result == -1) {
+                    pubUtil.upRecordInit.setFlag(-1);
+                    pubUtil.upRecordInit.setMsg("其他错误");
+                } else if (result == -2) {
+                    pubUtil.upRecordInit.setFlag(-1);
+                    pubUtil.upRecordInit.setMsg("account有误");
                 }
-                Log.i("init:",cjupRecordInitObj.getResult()+"");
+                Log.i("init:", pubUtil.upRecordInit.getResult() + "");
             }
         });
+        pubUtil.upRecordInit.setResult(result);
+        Log.i("end test",pubUtil.upRecordInit.getResult()+"end");
  //       return  cjupRecordInitObj;
     }
 }
