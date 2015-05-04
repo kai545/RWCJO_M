d150211b69d382ebbab9b04be160bcacfde3f262
Administrator <lili.cheng@awit.net.cn>
2015-05-04 10:30:59 +0800

    增加原始数据上传的类以及修改log语句的输出内容

--- app/src/main/java/rwcjom/awit/com/rwcjo_m/implInterfaces/CJUpOriExtends.java
+++ app/src/main/java/rwcjom/awit/com/rwcjo_m/implInterfaces/CJUpOriExtends.java
@@ -3,22 +3,22 @@ package rwcjom.awit.com.rwcjo_m.implInterfaces;
 import android.util.Log;
 
 import com.mor.dataacquisition.net.DataCallBack;
-import com.mor.dataacquisition.net.dataCallBacks.CJUpResultDataCallBack;
+import com.mor.dataacquisition.net.dataCallBacks.CJUpOriginalDataCallBack;
 import com.mor.dataacquisition.net.parsedData.CJResutResult;
 
 /**
  * Created by Administrator on 15-5-4.
  */
-public class CJUpResultExtends extends CJUpResultDataCallBack{
+public class CJUpOriExtends extends CJUpOriginalDataCallBack {
     private DataCallBack dataCallBack;
-    public CJUpResultExtends(DataCallBack dataCallBack){
+    public CJUpOriExtends(DataCallBack dataCallBack) {
         this.dataCallBack=dataCallBack;
     }
 
     @Override
     public void processData(CJResutResult data) {
         super.processData(data);
-        Log.i("upresult result",data.returnCode+"");
+        Log.i("original result",data.returnCode+"");
         dataCallBack.processData(data);
     }
 }
